1446266e3a5df02931825b1d6b001d146636a53c
adityareginold <adityareginold@gmail.com>
2024-03-04 17:41:39 +0530

    second

--- communitybackend/projectnew/frontend/src/components/NavBar2.jsx
+++ communitybackend/projectnew/frontend/src/components/NavBar2.jsx
@@ -47,6 +47,9 @@ const NavBar2 = () => {
                             <Dropdown.Item to= "/login"onClick={handleLogout}>Logout</Dropdown.Item>
                         </Dropdown.Menu>
                     </Dropdown> */}
+
+
+                    
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
